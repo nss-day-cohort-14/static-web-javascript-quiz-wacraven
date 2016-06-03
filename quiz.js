00c2385a2640c886b6b5555a60c1f13eb7a0e552
Alex Craven <w.alex.craven@gmail.com>
2016-06-03 11:18:18 -0500

    corrected object argument requirement

--- quiz.js
+++ quiz.js
@@ -1,25 +1,38 @@
 var treeObject = {};
-document.getElementById("growTreeBtn").addEventListener("click", tree);
+
+/*****************EVENT LISTENERS**************************/
+
+document.getElementById("growTreeBtn").addEventListener("click", clickListener);
 document.getElementById("character").addEventListener("keydown", enterListener);
 document.getElementById("treeHeight").addEventListener("keydown", enterListener);
 
 function enterListener(key) {
 	if (13 == key.keyCode) {
-		tree()
+		tree(treeObject);
 	}
 }
 
+function clickListener() {
+	tree(treeObject);
+}
 
-function tree() {
-	treeObject.hgt = document.getElementById('treeHeight').value;
-	treeObject.char = document.getElementById('character').value;
-	var treeLayer = treeObject.char;
-	var spacer = " ";
-	var center = Math.ceil(treeObject.hgt / 2);
-	console.log(spacer.repeat(center + parseInt(treeObject.hgt)),treeLayer);
+/*****************TREE BUILD**************************/
 
-	for (var i = 0; i <= treeObject.hgt - 2; i++) {
-		treeLayer = treeLayer + treeObject.char + treeObject.char;
-		console.log(spacer.repeat((center - i) + parseInt(treeObject.hgt)) + treeLayer);
+function tree(obj) {
+	obj.hgt = Math.abs(document.getElementById('treeHeight').value);
+	obj.char = document.getElementById('character').value;
+	if (obj.hgt == 0) {
+
+	} else {
+		var treeLayer = obj.char;
+		var spacer = " ";
+		var center = Math.ceil(obj.hgt / 2);
+		console.log(spacer.repeat(center + parseInt(obj.hgt)/2),treeLayer);
+
+		for (var i = 0; i <= obj.hgt - 2; i++) {
+			treeLayer = treeLayer + obj.char + obj.char;
+			console.log(spacer.repeat((center - i) + parseInt(obj.hgt)/2) + treeLayer);
+		}
 	}
+	console.log("treeObject", treeObject);
 }
